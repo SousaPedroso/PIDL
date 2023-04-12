582aa2031903fea5bec429d7df96091053a0c092
SousaPedroso <gsousa844@gmail.com>
2023-04-12 18:50:52 -0400

    preprocessing: mixup operation

--- panns/utils/utilities.py
+++ panns/utils/utilities.py
@@ -36,3 +36,17 @@ def move_data_to_device(x, device):
         return x
 
     return x.to(device)
+
+def do_mixup(x, mixup_lambda):
+    """Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes 
+    (1, 3, 5, ...).
+    Args:
+      x: (batch_size * 2, ...)
+      mixup_lambda: (batch_size * 2,)
+    Returns:
+      out: (batch_size, ...)
+    """
+    out = (x[0 :: 2].transpose(0, -1) * mixup_lambda[0 :: 2] + \
+        x[1 :: 2].transpose(0, -1) * mixup_lambda[1 :: 2]).transpose(0, -1)
+    return out
+    
